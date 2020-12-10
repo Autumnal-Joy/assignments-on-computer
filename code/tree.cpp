fc09e944c6ededa53c88320953c5648e112b1521
dcy <67720953+dachuanxd@users.noreply.github.com>
2020-12-10 22:10:17 +0800

    有bug

--- code/tree.cpp
+++ code/tree.cpp
@@ -27,43 +27,15 @@ void leftRotate(Tree*& tree) {}
 /* 右顺时针旋转, 更新双向指针的关系以及左右孩子高度 */
 void rightRotate(Tree*& tree) {}
 
-void balanced(Tree*& tree) {
-    int left = tree->leftChildHeight;
-    int right = tree->rightChildHeight;
-    if (left - right == 2) {
-        int left = tree->leftChild->leftChildHeight;
-        int right = tree->leftChild->rightChildHeight;
-        if (left - right == 1) {
-            // LL
-            rightRotate(tree);
-        } else if (left - right == -1) {
-            // LR
-            leftRotate(tree->leftChild);
-            rightRotate(tree);
-        }
-    } else if (left - right == -2) {
-        int left = tree->rightChild->leftChildHeight;
-        int right = tree->rightChild->rightChildHeight;
-        if (left - right == -1) {
-            // RR
-            leftRotate(tree);
-        } else if (left - right == 1) {
-            // RL
-            rightRotate(tree->rightChild);
-            leftRotate(tree);
-        }
-    }
-}
+void balanced(Tree*& tree) {}
 
 void insert(Tree*& tree, int key) {
     if (!tree) {
         tree = createTree(NULL, key);
     } else {
         if (key < tree->key) {
-            tree->leftChildHeight++;
             insert(tree->leftChild, key);
         } else {
-            tree->rightChildHeight++;
             insert(tree->rightChild, key);
         }
         balanced(tree);
