6a6cdb68e8dd298b3f44df30fcda417bf6aa00c9
dcy <67720953+dachuanxd@users.noreply.github.com>
2020-12-11 23:46:27 +0800

    lab6代码

--- code/main.c
+++ code/main.c
@@ -1,1 +1,22 @@
-int main() {}
+#include <sort.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const* argv[]) {
+    FILE* fp = fopen("test/test.in", "r");
+    int a = 0, b = 0, i = 0;
+    fscanf(fp, "%d", &b);
+    int data[b + 1];
+    while (i <= b) {
+        fscanf(fp, "%d", data[i]);
+        i++;
+    }
+    if (b < 19) {
+        insert(data, a, b);
+    }
+    else{
+        quick(data, a, b);
+        threequick(data, a, b);
+    }
+    return 0;
+}
