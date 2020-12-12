3465f3734752af8623537fe7eb3d5b95fcb4de38
dcy <67720953+dachuanxd@users.noreply.github.com>
2020-12-12 20:13:52 +0800

    代码

--- code/main.c
+++ code/main.c
@@ -3,20 +3,11 @@
 #include <string.h>
 
 int main(int argc, char const* argv[]) {
-    FILE* fp = fopen("test/test.in", "r");
+    FILE* fp = fopen("code/input.in", "r");
     int a = 0, b = 0, i = 0;
     fscanf(fp, "%d", &b);
     int data[b + 1];
-    while (i <= b) {
-        fscanf(fp, "%d", data[i]);
-        i++;
-    }
-    if (b < 19) {
-        insert(data, a, b);
-    }
-    else{
-        quick(data, a, b);
-        threequick(data, a, b);
-    }
+    quick(data, a, b);
+    threequick(data, a, b);
     return 0;
 }
--- code/sort.cpp
+++ code/sort.cpp
@@ -1,68 +1,54 @@
 #include <stdio.h>
 
 /*
-该函数输入为整型数组，待排数组的起点a和终点b。
+该函数输入为整型数组，待排数组的起点a和终点b(左闭右开区间)。
 然后进入循环，从待排数组的第二个元素开始扫描，若它大于或等于前一个元素，则继续扫描。
 否则在前边找到该元素的插入点，移动数组，进行插入。然后继续扫描。扫描到终点后，该数组的插入排序也就完成了。
 */
 void insert(int data[], int a, int b) {
-    int i = 1;
-    int j, k, sum;
-    while (i <= b) {
-        if (data[i] >= data[i - 1]) {
-            i++;
-        } else {
-            for (j = 0; j < i; j++) {
-                if (data[i] < data[j]) {
-                    sum = data[i];
-                    for (k = i; k != j; k--) {
-                        data[k] = data[k - 1];
-                    }
-                    data[j] = sum;
-                }
-            }
-            i++;
+    int i = a + 1;
+    int j, sum;
+    while (i < b) {
+        sum = data[i];
+        for (j = i; j > a && data[j - 1] >= data[j]; j--) {
+            data[j] = data[j - 1];
         }
-    }
-    for (i = 0; i <= b; i++) {
-        printf("%d ", data[i]);
+        data[j] = sum;
+        i++;
     }
 }
 
 /*
-该函数输入为整型数组，a, b分别为待排数组的起始点和终点。
-然后将data[a]作为枢轴元素。定义两个指针startptr和endptr，
-指向待排数组的两端，并且向中间靠拢。当startptr遇到比枢轴元素大的数，
-endptr遇到比枢轴元素小的数，则这两个数交换位置。直到两指针相遇为止，
+该函数输入为整型数组，a, b分别为待排数组的起始点和终点(左闭右开区间)。
+然后将data[a]作为枢轴元素。定义两个指针start和end，
+指向待排数组的两端，并且向中间靠拢。当start遇到比枢轴元素大的数，
+end遇到比枢轴元素小的数，则这两个数交换位置。直到两指针相遇为止，
 然后将枢轴元素赋值给两指针相遇的位置，即枢轴元素在待排序列中的正确位置。
 然后调用两次本函数，分别对枢轴元素左边和右边的待排数组进行同样的操作，
-不断递归，直到只剩一个数待排序为止。
+不断递归，当待排子序列长度已小于20时进行插入排序。
 */
 void quick(int data[], int a, int b) {
-    if (a == b)
+    if (b - a < 20) {
+        insert(data, a, b);
         return;
+    }
     int key = data[a];
-    int startptr = a;
-    int endptr = b;
-    int i, j, sum;
-    while (startptr != endptr) {
-        if (data[startptr] > key) {
-            for (; startptr != endptr; endptr--) {
-                if (data[endptr] < key) {
-                    sum = data[startptr];
-                    data[startptr] = data[endptr];
-                    data[endptr] = sum;
-                    break;
-                }
-            }
+    int start = a;
+    int end = b - 1;
+    int sum;
+    while (start < end) {
+        while (start < end && data[start] <= key) {
+            ++start;
         }
-        startptr++;
+        while (start < end && data[end] >= key) {
+            --end;
+        }
+        sum = data[start];
+        data[start] = data[end];
+        data[end] = sum;
     }
-    quick(data, a, startptr);
-    quick(data, startptr, b);
-    for (i = 0; i <= b; i++) {
-        printf("%d ", data[i]);
-    }
+    quick(data, a, start);
+    quick(data, start, b);
 }
 
 /*
@@ -72,8 +58,10 @@ void quick(int data[], int a, int b) {
 然后将其和第一个元素交换位置，其余函数和快排函数一致。
 */
 void threequick(int data[], int a, int b) {
-    if (a == b)
+    if (b - a < 20) {
+        insert(data, a, b);
         return;
+    }
     int key;  //枢轴
     if (data[(a + b) / 2] >= a && data[(a + b) / 2] <= b) {
         key = data[(a + b) / 2];
@@ -82,25 +70,20 @@ void threequick(int data[], int a, int b) {
     } else {
         key = b;
     }
-    int startptr = a;
-    int endptr = b;
-    int i, j, sum;
-    while (startptr != endptr) {
-        if (data[startptr] > key) {
-            for (; startptr != endptr; endptr--) {
-                if (data[endptr] < key) {
-                    sum = data[startptr];
-                    data[startptr] = data[endptr];
-                    data[endptr] = sum;
-                    break;
-                }
-            }
+    int start = a;
+    int end = b - 1;
+    int sum;
+    while (start < end) {
+        while (start < end && data[start] <= key) {
+            ++start;
         }
-        startptr++;
+        while (start < end && data[end] >= key) {
+            --end;
+        }
+        sum = data[start];
+        data[start] = data[end];
+        data[end] = sum;
     }
-    quick(data, a, startptr);
-    quick(data, startptr, b);
-    for (i = 0; i <= b; i++) {
-        printf("%d ", data[i]);
-    }
+    threequick(data, a, start);
+    threequick(data, start, b);
 }
